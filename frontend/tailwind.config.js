e1feeca72f0eaf2a93dc1edcb54c069ac89caefb
iamrayghazali <ghazali.raydan@gmail.com>
2026-07-30 02:41:35 +0200

    Update TW colors

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    theme: {
+        extend: {
+            colors: {
+                bg: {
+                    primary: "#F6F2EB",
+                    secondary: "#E7DED2",
+                },
+                eucalyptus: {
+                    light: "#708A74",
+                    dark: "#58705C",
+                },
+                text: {
+                    primary: "#3A332D",
+                },
+                cta: "#A67C52",
+            },
+        },
+    },
+}
