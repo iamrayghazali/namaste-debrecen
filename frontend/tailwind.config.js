9681860daaabf8404eafef3f5c59a7322b71ab02
iamrayghazali <151096230+iamrayghazali@users.noreply.github.com>
2026-08-13 14:48:51 +0200

    Add dependencies

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,21 +1,2 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-    theme: {
-        extend: {
-            colors: {
-                bg: {
-                    primary: "#F6F2EB",
-                    secondary: "#E7DED2",
-                },
-                eucalyptus: {
-                    light: "#708A74",
-                    dark: "#58705C",
-                },
-                text: {
-                    primary: "#3A332D",
-                },
-                cta: "#A67C52",
-            },
-        },
-    },
-}
+export default {}
